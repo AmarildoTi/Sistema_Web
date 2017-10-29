a9bb41ffdbb3948f689e913759c4940d56391ec2
Amarildo <d_amarildo@hotmail.com>
2017-10-29 14:29:21 -0200

    Atualização 29/10/2017

--- src/main/java/br/com/amarildo/view/EnvioEmailBean.java
+++ src/main/java/br/com/amarildo/view/EnvioEmailBean.java
@@ -1,14 +1,19 @@
 package br.com.amarildo.view;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
 
+import br.com.amarildo.model.FuncionarioModel;
+import br.com.amarildo.model.LogradouroModel;
 import br.com.amarildo.model.PessoaModel;
+import br.com.amarildo.model.UsuarioModel;
 import br.com.amarildo.util.email.Mailer;
 import br.com.amarildo.util.mensagens.NegocioException;
 
@@ -20,6 +25,16 @@ import br.com.amarildo.util.mensagens.NegocioException;
 		private static final long serialVersionUID = 1L;
 
 		@Inject
+		private FuncionarioModel funcionarioModel;
+
+		@Inject
+		private LogradouroModel logradouroModel;
+
+		@Inject
+		private UsuarioModel usuarioModel;
+
+		
+		@Inject
 		private Mailer mailer;
 		
 		public void enviarEmail(PessoaModel pessoaModel) {
@@ -30,7 +45,9 @@ import br.com.amarildo.util.mensagens.NegocioException;
 			
 			message.to(pessoaModel.getEmail())
 				.subject(" Assunto do E-mail, Teste de envio ")
-				.bodyHtml("<strong>Mensagem do Corpo do E-mail Teste de Envio de E-mail </strong> ")
+				//.bodyHtml("<strong>Mensagem do Corpo do E-mail Teste de Envio de E-mail </strong> ")
+				.bodyHtml(new VelocityTemplate(getClass().getResourceAsStream("/br/com/amarildo/mensagens/CorpoEmail.template")))
+				.put("pessoaModel", pessoaModel)
 				.send();
 		
 			NegocioException.MensagemInformacao("e-mail enviado com sucesso!! para "+pessoaModel.getEmail());
